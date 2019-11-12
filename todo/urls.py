1a4dc2cb17dad10e24001e8d75881d552f91d163
Ljupcho Apostolov <ljupcho.apostolov@gmail.com>
2019-11-12 16:14:16 +0100

    adding new folder structure due to django 1.11 version

--- todo/urls.py
+++ todo/urls.py
@@ -1,17 +1,22 @@
-from django.conf.urls import patterns, include, url
+"""todo URL Configuration
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'todo.views.home', name='home'),
-    # url(r'^todo/', include('todo.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    # url(r'^admin/', admin.site.urls),
+    url(r'^todolist/', include('todolist.urls')),
+]
