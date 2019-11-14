af6718949401e84cc98df1f2350bb9053f1aec41
Ljupcho Apostolov <ljupcho@1022000332.local>
2019-11-14 14:08:39 +0100

    adding tests for category model

--- todolist/tests.py
+++ todolist/tests.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+from todolist.models import Category
+
+
+class CategoryModelTest(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		Category.objects.create(name='test')
+
+	def test_category_name(self):
+		category = Category.objects.get(name='test')
+		self.assertEqual(category.name, 'test')
+
+	def test_get_absolute_url(self):
+		category = Category.objects.get(name='test')
+		self.assertEqual(category.get_absolute_url(), '/todolist/categories/'+ str(category.id) +'/')
